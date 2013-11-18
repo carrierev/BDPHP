9e2b98a7d5c9f65209bc634249cba7c11ba62834
northvik <camillepire@northvik.fr>
2013-11-18 13:18:58 +0100

    creation du fichier include

--- bdphp.php
+++ bdphp.php
@@ -5,10 +5,9 @@
 // Login   <carrie_v@etna-alternance.net>
 //
 // Started on  Mon Nov 18 10:30:38 2013 Valentin Carriere
-// Last update Mon Nov 18 13:13:10 2013 camille pire
+// Last update Mon Nov 18 13:17:50 2013 camille pire
 //
-require_once('./include/affichage.php');
-require_once('./include/cut.php');
+require_once('./include/include.php');
 error_reporting (E_ALL);
 
 $fd = fopen('php://stdin', 'r');
