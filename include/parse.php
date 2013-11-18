821be5464667b1984e49d3bc2412d7c32a313da7
northvik <camillepire@northvik.fr>
2013-11-18 17:12:38 +0100

    lower

--- include/parse.php
+++ include/parse.php
@@ -5,12 +5,11 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:03:13 2013 camille pire
-// Last update Mon Nov 18 16:52:49 2013 camille pire
+// Last update Mon Nov 18 17:10:32 2013 camille pire
 //
 
 function	parse_sql(&$param)
 {
-  for ($i = 0; isset($param[$i]);$i++)
-    echo $param[0];
+  print_r($param);
   return ;
 }
