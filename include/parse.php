7c702ba325b3f13505c387331b8db60598186828
northvik <camillepire@northvik.fr>
2013-11-18 23:05:26 +0100

    maj

--- include/parse.php
+++ include/parse.php
@@ -5,11 +5,13 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:03:13 2013 camille pire
-// Last update Mon Nov 18 17:10:32 2013 camille pire
+// Last update Mon Nov 18 19:58:48 2013 camille pire
 //
 
 function	parse_sql(&$param)
 {
+  if (in_array(";", $param))
+    return ;
   print_r($param);
   return ;
 }
