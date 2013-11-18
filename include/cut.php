97784c8a6cb0c46d1bb80635ee7806ea5595f409
carrierev <valentin.carriere@gmail.com>
2013-11-18 10:42:13 +0100

    test

---
+++ include/cut.php
@@ -0,0 +1,13 @@
+<?php
+// cut.php for  in /Users/valentin/Desktop/ProjetPHP/BDPHP/include
+// 
+// Made by Valentin Carriere
+// Login   <carrie_v@etna-alternance.net>
+// 
+// Started on  Mon Nov 18 10:39:51 2013 Valentin Carriere
+// Last update Mon Nov 18 10:40:34 2013 Valentin Carriere
+//
+function        decoup_params($line)
+{
+  return (explode(' ', $line));
+}
