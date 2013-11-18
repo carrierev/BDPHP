10b99db3f4e7553b1ed44575ef12d98bbaa9abdf
northvik <camillepire@northvik.fr>
2013-11-18 16:43:05 +0100

    gestion des minuscule pour quit

--- bdphp.php
+++ bdphp.php
@@ -1,11 +1,11 @@
-<?php
+c<?php
 // bdphp.php for  in /Users/valentin/Desktop/ProjetPHP/BDPHP
 //
 // Made by Valentin Carriere
 // Login   <carrie_v@etna-alternance.net>
 //
 // Started on  Mon Nov 18 10:30:38 2013 Valentin Carriere
-// Last update Mon Nov 18 16:39:30 2013 camille pire
+// Last update Mon Nov 18 16:42:40 2013 camille pire
 //
 require_once('./include/include.php');
 error_reporting (E_ALL);
@@ -17,7 +17,7 @@ if ($fd !== false)
     while (($line = fgets($fd)) !== false)
       {
 	$params = decoup_params($line);
-	if (strcmp($params[0], "QUIT;") == 1 || $params[0] == "QUIT;")
+	if (strcmp(strtolower($params[0]), "quit;") == 1 || $params[0] == "QUIT;")
 	  exit;
 	aff_prompt();
       }
