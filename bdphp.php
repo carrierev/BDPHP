ee6fbaea03bafd0cbec46bed49c5bf5973628f0e
northvik <camillepire@northvik.fr>
2013-11-18 13:13:58 +0100

    mise a la norme :)

--- bdphp.php
+++ bdphp.php
@@ -1,15 +1,16 @@
 <?php
 // bdphp.php for  in /Users/valentin/Desktop/ProjetPHP/BDPHP
-// 
+//
 // Made by Valentin Carriere
 // Login   <carrie_v@etna-alternance.net>
-// 
+//
 // Started on  Mon Nov 18 10:30:38 2013 Valentin Carriere
-// Last update Mon Nov 18 10:40:12 2013 Valentin Carriere
+// Last update Mon Nov 18 13:13:10 2013 camille pire
 //
 require_once('./include/affichage.php');
 require_once('./include/cut.php');
 error_reporting (E_ALL);
+
 $fd = fopen('php://stdin', 'r');
 if ($fd !== false)
   {
