4f071aa88ecb0b146b43d00f45947b35c162cdc5
northvik <camillepire@northvik.fr>
2013-11-19 13:05:09 +0100

    write

--- include/write.php
+++ include/write.php
@@ -5,5 +5,11 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:03:42 2013 camille pire
-// Last update Mon Nov 18 16:03:52 2013 camille pire
+// Last update Tue Nov 19 13:04:57 2013 camille pire
 //
+
+//ecriture dans la base
+
+function	write_db()
+{
+}
