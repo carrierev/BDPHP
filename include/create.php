5dda21df4efc3eb9fd733f83846c599c1317c78a
northvik <camillepire@northvik.fr>
2013-11-19 13:03:51 +0100

    preparation de l ecriture

--- include/create.php
+++ include/create.php
@@ -5,7 +5,7 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Tue Nov 19 12:38:23 2013 camille pire
-// Last update Tue Nov 19 12:57:33 2013 camille pire
+// Last update Tue Nov 19 13:01:09 2013 camille pire
 //
 
 //cree un fichier avec le nom passer en paramettre dans
@@ -15,4 +15,3 @@ function	file_create($path, $name)
 {
   touch($path . $name);
 }
-file_create('../database/table/', 'test.table');
--- include/include.php
+++ include/include.php
@@ -5,7 +5,7 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 13:16:09 2013 camille pire
-// Last update Tue Nov 19 11:52:34 2013 Valentin Carriere
+// Last update Tue Nov 19 13:03:40 2013 camille pire
 //
 
 // include des differents fichier php
@@ -17,3 +17,4 @@ require_once('./include/drop.php');
 require_once('./include/show.php');
 require_once('./include/write.php');
 require_once('./include/check_argv.php');
+require_once('./include/create.php');
