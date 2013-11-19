3b43bab07463278a623bcc676c4dce6a30a400c5
northvik <camillepire@northvik.fr>
2013-11-19 13:00:11 +0100

    maj

--- bdphp.php
+++ bdphp.php
@@ -5,24 +5,14 @@
 // Login   <carrie_v@etna-alternance.net>
 //
 // Started on  Mon Nov 18 10:30:38 2013 Valentin Carriere
-<<<<<<< HEAD
-// Last update Tue Nov 19 12:53:20 2013 camille pire
-//
+// Last update Tue Nov 19 13:00:03 2013 camille pire
 require_once('./include/include.php');
 error_reporting (E_ALL);
 
 if (is_options($argc, $argv) == 0)
   exit (0);
-=======
-// Last update Tue Nov 19 12:55:08 2013 Valentin Carriere
-//
-require_once('./include/include.php');
-error_reporting (E_ALL);
-/*if (is_options($argc, $argv) == 0)
-  exit;
->>>>>>> cb94c368ad299e6d404666cb9c4b00e31f04e558
 else
-$file_options = is_options($argc, $argv);*/
+  $file_options = is_options($argc, $argv);
 $fd = fopen('php://stdin', 'r');
 if ($fd !== false)
   {
