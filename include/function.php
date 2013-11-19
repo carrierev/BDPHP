a065a843f6912f362882ab65d767560ed0a5de45
northvik <camillepire@northvik.fr>
2013-11-19 17:50:47 +0100

    create table

--- include/function.php
+++ include/function.php
@@ -5,7 +5,7 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Tue Nov 19 16:38:38 2013 camille pire
-// Last update Tue Nov 19 17:27:05 2013 camille pire
+// Last update Tue Nov 19 17:50:18 2013 camille pire
 //
 
 function	createTab($cmd, $file)
@@ -20,7 +20,12 @@ function	createTab($cmd, $file)
 	  $dbline .= $cmd[$i] . ';';
 	else
 	  $dbline .= $cmd[$i] . ',';
+      if ($cmd[$i - 1] == '(' || $cmd[$i - 1] == ',')
+	$tbline .= $cmd[$i] . ";";
     }
   $dbline .= './database/table/' . $file_name . '#';
+  if (isset($tbline))
+    write_tb($file_name, $tbline);
   write_db($file[0], $dbline);
+  echo "-> Table " . "'" . $cmd[2] . "'" . " created.\n";
 }
