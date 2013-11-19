573a298c6cbf928cc6f7d5be4d16a4cddd4fdc36
northvik <camillepire@northvik.fr>
2013-11-19 10:02:45 +0100

    point virgule gerer

--- bdphp.php
+++ bdphp.php
@@ -5,7 +5,7 @@
 // Login   <carrie_v@etna-alternance.net>
 //
 // Started on  Mon Nov 18 10:30:38 2013 Valentin Carriere
-// Last update Tue Nov 19 09:43:42 2013 camille pire
+// Last update Tue Nov 19 09:56:59 2013 camille pire
 //
 require_once('./include/include.php');
 error_reporting (E_ALL);
@@ -19,7 +19,7 @@ if ($fd !== false)
 	$params = decoup_params($line);
 	if (strcmp($params[0], "quit;") == 1)
 	  exit;
-	parse_sql($params);
+	parse_sql($params, $fd);
 	aff_prompt();
       }
   }
--- include/parse.php
+++ include/parse.php
@@ -5,14 +5,20 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:03:13 2013 camille pire
-// Last update Tue Nov 19 09:46:52 2013 camille pire
+// Last update Tue Nov 19 10:02:12 2013 camille pire
 //
 
-function	parse_sql(&$param)
+function	parse_sql(&$param, $fd)
 {
-  if (!preg_match_all('#;$#', $param[count($param) - 1]))
-    return ;
-  else
-    print_r($param);
+  $in[0] = $param;
+
+
+  for ($i = 1; !preg_match_all('#;$#', $param[count($param) - 1]); $i++)
+    {
+      echo " > ";
+      $param = decoup_params(strtolower(fgets($fd)));
+      $in[$i] = $param;
+    }
+  print_r($in);
   return ;
 }
