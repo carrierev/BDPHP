2e1e0c09508af166838104e99aea604b970494ac
northvik <camillepire@northvik.fr>
2013-11-19 11:30:09 +0100

    parsing sql fini

--- include/cut.php
+++ include/cut.php
@@ -5,9 +5,10 @@
 // Login   <carrie_v@etna-alternance.net>
 //
 // Started on  Mon Nov 18 10:39:51 2013 Valentin Carriere
-// Last update Mon Nov 18 16:47:41 2013 camille pire
+// Last update Tue Nov 19 11:19:22 2013 camille pire
 //
 function        decoup_params($line)
 {
-  return (explode(' ', $line));
+  $res = preg_replace('#[ ]{2,}|[\t]#', ' ', $line);
+  return (explode(' ', $res));
 }
--- include/parse.php
+++ include/parse.php
@@ -5,13 +5,12 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:03:13 2013 camille pire
-// Last update Tue Nov 19 10:42:34 2013 camille pire
+// Last update Tue Nov 19 11:28:24 2013 camille pire
 //
 
 function	parse_sql(&$param, $fd)
 {
   $in[0] = $param;
-
   for ($i = 1; !preg_match_all('#;$#', $param[count($param) - 1], $tab); $i++)
     {
       echo " > ";
@@ -20,10 +19,15 @@ function	parse_sql(&$param, $fd)
     }
   for ($i = 0; isset($in[$i]); $i++)
     for ($j = 0; isset($in[$i][$j]); $j++)
+      {
+	$in[$i][$j] = preg_replace('#\\n$|;$#', '', $in[$i][$j]);
       if ($in[$i][$j] != ''
-	  && !preg_match_all('#^ +$#', $in[$i][$j], $tab)
-	  && !preg_match_all('#;$#', $in[$i][$j], $tab))
+	  && !preg_match_all('#^ +$#', $in[$i][$j], $tab))
 	$res[] = $in[$i][$j];
-  print_r($res);
-  return ;
+      }
+  if (isset($res))
+    {
+      print_r($res);
+      return $res;
+    }
 }
