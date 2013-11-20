2f1fbb9d65bfc2e93f8d1d1cce555f98a0416991
northvik <camillepire@northvik.fr>
2013-11-20 18:47:56 +0100

    tr

--- include/function.php
+++ include/function.php
@@ -5,8 +5,33 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Tue Nov 19 16:38:38 2013 camille pire
-// Last update Tue Nov 19 17:50:18 2013 camille pire
+// Last update Wed Nov 20 12:36:22 2013 camille pire
 //
+function test_type($str)
+{
+  if ($str == 'integer'
+      || $str == 'float'
+      || $str == 'bool'
+      || $str == 'string')
+    return true;
+  else
+    return false;
+}
+
+function	test_option($str)
+{
+  static $bool = false;
+
+  if ($bool == false && $str == 'primary_key')
+    {
+      $bool == true;
+      return true;
+    }
+  elseif ($str == 'not_null')
+    return true;
+  else
+    return false;
+}
 
 function	createTab($cmd, $file)
 {
@@ -15,13 +40,24 @@ function	createTab($cmd, $file)
   $dbline = $cmd[2] . ";";
   for ($i = 4; isset($cmd[$i + 1]); $i++)
     {
-      if ($cmd[$i] != ',')
-	if ($cmd[$i + 1] == ',' || $cmd[$i + 1] == ')')
-	  $dbline .= $cmd[$i] . ';';
-	else
-	  $dbline .= $cmd[$i] . ',';
       if ($cmd[$i - 1] == '(' || $cmd[$i - 1] == ',')
-	$tbline .= $cmd[$i] . ";";
+	{
+	  $dbline .= $cmd[$i] . ',';
+	  $tbline .= $cmd[$i] . ";";
+	}
+      elseif ($cmd[$i] != ',')
+	{
+	  if (($cmd[$i + 1] == ',' || $cmd[$i + 1] == ')')
+	      && test_option($cmd[$i]))
+	    $dbline .= $cmd[$i] . ';';
+	  elseif (test_type($cmd[$i]))
+	    $dbline .= $cmd[$i] . ',';
+	  else
+	    {
+	      echo "Invalid commande: '" . $cmd[$i] . "'\n";
+	      return ;
+	    }
+	}
     }
   $dbline .= './database/table/' . $file_name . '#';
   if (isset($tbline))
