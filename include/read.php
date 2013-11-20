546d6e40c4f62710b1d684a947d1f2a54050c00f
carrierev <valentin.carriere@gmail.com>
2013-11-20 10:45:30 +0100

    COMMIT

--- include/read.php
+++ include/read.php
@@ -5,7 +5,7 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:02:41 2013 camille pire
-// Last update Wed Nov 20 10:40:03 2013 Valentin Carriere
+// Last update Wed Nov 20 10:44:39 2013 Valentin Carriere
 //
 
 function	read_db($path)
@@ -25,4 +25,26 @@ function	showtab($line)
     }
   if (isset($tab) && isset($title))
     print_tab($title, $tab);
+}
+
+function        desctab($file, $cmd)
+{
+  $lines = read_db($file[0]);
+  for ($i = 0; isset($lines[$i]); $i++)
+    {
+      preg_match_all('#([^;]+);#', $lines[$i], $tab[$i]);
+      if ($tab[$i][0][0] == $cmd[1] . ';')
+        $res[] = $tab[$i][1];
+    }
+  if (isset($res))
+    {
+      $title = $res[0][0];
+      echo $title ."\n";
+      for ($i = 1; isset($res[0][$i]); $i++)
+        {
+	  preg_match_all('#([^,]+)#', $res[0][$i], $tab2[$i]);
+	  $table[] = $tab2[$i][1];
+        }
+    }
+  print_r($table);
 }
