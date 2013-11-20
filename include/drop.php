aacdba2b7b67e1340a511ae3ca36b424d0d7b661
carrierev <valentin.carriere@gmail.com>
2013-11-20 11:05:27 +0100

    dqww

--- include/drop.php
+++ include/drop.php
@@ -5,12 +5,13 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:04:08 2013 camille pire
-// Last update Wed Nov 20 10:51:34 2013 Valentin Carriere
+// Last update Wed Nov 20 11:04:17 2013 Valentin Carriere
 //
 function	drop($file, $cmd)
 {
+  /*
   $ptr = fopen("./" . $file[0], "r");
-  $contenu = fread($ptr, filesize("./" . $file[0]));
+  $contenu = fread($ptr, filesize($file[0]));
   fclose($ptr);
   $contenu = explode(PHP_EOL, $contenu);
   for ($i = 0; isset($contenu[$i]); $i++)
@@ -23,4 +24,16 @@ function	drop($file, $cmd)
   $contenu = implode(PHP_EOL, $contenu);
   $ptr = fopen("./" . $file[0], "w");
   fwrite($ptr, $contenu);
+  */
+  $lines = file($file[0]);
+  // return $lines;
+  //$lines = read_db($file[0]);
+  print_r($lines);
+  for ($i = 0; isset($lines[$i]); $i++)
+    {
+      if (preg_match_all('#([' . $cmd[1] . ']+)##', $lines[$i], $res))
+	if ($res != '')
+	  unset($file[0][$i]);
+    }
+
 }
