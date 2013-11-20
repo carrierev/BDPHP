61f4b2b09bdf9d42462ad99837913ac36d22493b
carrierev <valentin.carriere@gmail.com>
2013-11-20 11:51:54 +0100

    Move echo tab & print_tab to show.php

--- include/affichage.php
+++ include/affichage.php
@@ -5,7 +5,7 @@
 // Login   <carrie_v@etna-alternance.net>
 //
 // Started on  Mon Nov 18 10:31:24 2013 Valentin Carriere
-// Last update Wed Nov 20 11:11:20 2013 camille pire
+// Last update Wed Nov 20 11:48:47 2013 Valentin Carriere
 //
 
 function        aff_prompt()
@@ -27,36 +27,3 @@ function	multi_echo_ret($char, $nb)
   return $tmp;
 }
 
-function	echo_tab($tab, $size, $option)
-{
-  for ($i = 0; isset($tab[$i]) && $option == 'simple'; $i++)
-    {
-      echo "| " . $tab[$i];
-      multi_echo(' ', ($size - strlen($tab[$i]) + 1));
-      echo " |\n";
-    }
-}
-
-function	print_tab($title, $tab)
-{
-  $size = strlen($title);
-  for ($i = 0; isset($tab[$i]); $i++)
-    if (strlen($tab[$i]) > $size)
-      $size = strlen($tab[$i]);
-  echo "+";
-  multi_echo('-', $size + 3);
-  echo "+\n| ";
-  multi_echo(' ',($size - strlen($title)) / 2);
-  echo $title;
-  multi_echo(' ',($size - strlen($title)) / 2);
-  if ($size % 2)
-    echo ' ';
-  echo " |\n";
-  echo "+";
-  multi_echo('-', $size + 3);
-  echo "+\n";
-  echo_tab($tab, $size, 'simple');
-  echo "+";
-  multi_echo('-', $size + 3);
-  echo "+\n" . $i ." rows in set\n";
-}
--- include/show.php
+++ include/show.php
@@ -5,5 +5,40 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 16:04:48 2013 camille pire
-// Last update Mon Nov 18 16:04:56 2013 camille pire
+// Last update Wed Nov 20 11:48:36 2013 Valentin Carriere
 //
+
+function        echo_tab($tab, $size, $option)
+{
+  for ($i = 0; isset($tab[$i]) && $option == 'simple'; $i++)
+    {
+      echo "| " . $tab[$i];
+      multi_echo(' ', ($size - strlen($tab[$i]) + 1));
+      echo " |\n";
+    }
+}
+
+function        print_tab($title, $tab)
+{
+  $size = strlen($title);
+  for ($i = 0; isset($tab[$i]); $i++)
+    if (strlen($tab[$i]) > $size)
+      $size = strlen($tab[$i]);
+  echo "+";
+  multi_echo('-', $size + 3);
+  echo "+\n| ";
+  multi_echo(' ',($size - strlen($title)) / 2);
+  echo $title;
+  multi_echo(' ',($size - strlen($title)) / 2);
+  if ($size % 2)
+    echo ' ';
+  echo " |\n";
+  echo "+";
+  multi_echo('-', $size + 3);
+  echo "+\n";
+  echo_tab($tab, $size, 'simple');
+  echo "+";
+  multi_echo('-', $size + 3);
+  echo "+\n" . $i ." rows in set\n";
+}
+
