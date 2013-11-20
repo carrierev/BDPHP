d3f98ce638ca2ce7bb78d4271ada65da458963c2
carrierev <valentin.carriere@gmail.com>
2013-11-20 14:59:45 +0100

    Truncate good to go (avec gestion d'erreurs bitte schön)

--- include/truncate.php
+++ include/truncate.php
@@ -5,7 +5,7 @@
 // Login   <carrie_v@etna-alternance.net>
 // 
 // Started on  Wed Nov 20 12:27:00 2013 Valentin Carriere
-// Last update Wed Nov 20 14:55:20 2013 Valentin Carriere
+// Last update Wed Nov 20 14:58:31 2013 Valentin Carriere
 //
 function        truncate($file, $cmd)
 {
@@ -32,21 +32,17 @@ function        writeTable($cmd, $file)
   $contenu = fread($ptr, filesize("./database/table/" . $cmd[1] . ".table"));
   fclose($ptr);
   $contenu = explode(PHP_EOL, $contenu);
-  print_r($contenu);
- for ($i = 1; isset($contenu[$i]); $i++)
+  if (!isset($contenu[1]))
     {
-      unset($contenu[$i]);
+      echo "Table : '" . $cmd[1] . "' already empty\n";
+      return (0);
     }
+for ($i = 1; isset($contenu[$i]); $i++)
+    unset($contenu[$i]);
   $contenu = array_values($contenu);
   $contenu = implode(PHP_EOL, $contenu);
   $ptr = fopen("./database/table/" . $cmd[1] . ".table", "w");
   fwrite($ptr, $contenu);
   fclose($ptr);
-  /*$filename = "./database/table" . $cmd[1] . ".table";
-    $ptr = fopen($filename, "r+");
-    ftruncate($ptr,filesize($filename)));
-    rewind($ptr);
-    fclose($ptr);
-  */
   echo "Table : '" . $cmd[1] . "' truncated\n";
 }
