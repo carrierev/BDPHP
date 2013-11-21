6b1f2b974872f17ccd9e53ce425631361b085c84
carrierev <valentin.carriere@gmail.com>
2013-11-21 17:20:27 +0100

    OWUDOWD

--- include/include.php
+++ include/include.php
@@ -5,12 +5,13 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Mon Nov 18 13:16:09 2013 camille pire
-// Last update Thu Nov 21 15:14:41 2013 Valentin Carriere
+// Last update Thu Nov 21 17:20:21 2013 Valentin Carriere
 //
 
 // include des differents fichier php
+
 require_once('./include/input_file.php');
-require_once('./include/clear.php');
+require_once('./include/input_file.php');
 require_once('./include/delete.php');
 require_once('./include/truncate.php');
 require_once('./include/help.php');
@@ -25,3 +26,4 @@ require_once('./include/check_argv.php');
 require_once('./include/create.php');
 require_once('./include/user_choice.php');
 require_once('./include/function.php');
+require_once('./include/insert.php');
