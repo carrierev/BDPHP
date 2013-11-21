6ba1176abd402ac8fde34b4ff3a667aa6c54fbb6
carrierev <valentin.carriere@gmail.com>
2013-11-21 17:19:11 +0100

    UEQ
    QYSQOUSQO

---
+++ include/input_file.php
@@ -0,0 +1,24 @@
+<?php
+// input_file.php for  in /Users/valentin/Desktop/ProjetPHP/BDPHP/include
+// 
+// Made by Valentin Carriere
+// Login   <carrie_v@etna-alternance.net>
+// 
+// Started on  Thu Nov 21 15:01:03 2013 Valentin Carriere
+// Last update Thu Nov 21 17:16:55 2013 Valentin Carriere
+//
+function	input_file($file)
+{
+  $ptr = fopen($file[1], "r");
+  $contenu = fread($ptr, filesize($file[1]));
+  if (preg_match_all('#[^;]+;#', $contenu, $tab))
+    {
+      for ($i = 0; isset($tab[0][$i]); $i++)
+	{
+	  $tab[0][$i] = decoup_params($tab[0][$i]);
+	  $cmd = parse_sql($tab[0][$i], $ptr);
+	  print_r($cmd);
+	  user_choice($cmd, $file);
+	}
+    }
+}
