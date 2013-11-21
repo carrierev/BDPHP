a8134a1f5f4a7a8dbc773a86895c2cd85875322c
northvik <camillepire@northvik.fr>
2013-11-21 14:51:09 +0100

    creation du insert

---
+++ include/insert.php
@@ -0,0 +1,9 @@
+<?php
+// insert.php for insert in /Users/camillepire/github/BDPHP/include
+//
+// Made by camille pire
+// Login   <pire_c@etna-alternance.net>
+//
+// Started on  Thu Nov 21 14:50:33 2013 camille pire
+// Last update Thu Nov 21 14:50:47 2013 camille pire
+//
