baf2d0a5219f00bd318c9c302ea4d4032f82b4f0
northvik <camillepire@northvik.fr>
2013-11-21 13:57:51 +0100

    create table FINIIIIIIIIIIIII

--- include/function.php
+++ include/function.php
@@ -5,7 +5,7 @@
 // Login   <pire_c@etna-alternance.net>
 //
 // Started on  Tue Nov 19 16:38:38 2013 camille pire
-// Last update Wed Nov 20 12:36:22 2013 camille pire
+// Last update Thu Nov 21 13:57:31 2013 camille pire
 //
 function test_type($str)
 {
@@ -18,38 +18,80 @@ function test_type($str)
     return false;
 }
 
-function	test_option($str)
+function	test_option($str, $option)
 {
   static $bool = false;
 
-  if ($bool == false && $str == 'primary_key')
+  if ($option == 0 && $bool == false)
     {
-      $bool == true;
+      echo "You must set one primary_key.\n";
       return true;
     }
-  elseif ($str == 'not_null')
-    return true;
-  else
-    return false;
+  elseif ($option == 0)
+    $bool = false;
+  if ($option == 1)
+    {
+      if ($bool == false && $str == 'primary_key')
+	{
+	  $bool = true;
+	  return true;
+	}
+      elseif ($str == 'not_null')
+	return true;
+      elseif ($bool == true && $str == 'primary_key')
+	{
+	  echo "You can have just one primary_key.\n";
+	  return false;
+	}
+      else
+	return false;
+    }
+  return false;
+}
+
+function	iftab_exist($cmd, $file)
+{
+  $lines = read_db($file[0]);
+  for ($i = 1; isset($lines[$i]); $i++)
+    {
+      preg_match('#^[^;]+#', $lines[$i], $tab);
+	if ($tab[0] == $cmd[2])
+	  {
+	    echo "Table already exist.\n";
+	    return true;
+	  }
+    }
+  return false;
 }
 
 function	createTab($cmd, $file)
 {
+  if (iftab_exist($cmd, $file))
+    return ;
   $file_name = $cmd[2] . '.table';
   file_create('./database/table/', $file_name);
   $dbline = $cmd[2] . ";";
-  for ($i = 4; isset($cmd[$i + 1]); $i++)
+  $tbline = null;
+ $j = 0;
+ for ($i = 4; isset($cmd[$i + 1]); $i++)
     {
-      if ($cmd[$i - 1] == '(' || $cmd[$i - 1] == ',')
+      if ($cmd[$i - 1] == '(' || preg_match('#,$#', $cmd[$i - 1]))
 	{
 	  $dbline .= $cmd[$i] . ',';
 	  $tbline .= $cmd[$i] . ";";
 	}
       elseif ($cmd[$i] != ',')
 	{
-	  if (($cmd[$i + 1] == ',' || $cmd[$i + 1] == ')')
-	      && test_option($cmd[$i]))
-	    $dbline .= $cmd[$i] . ';';
+	  if ($cmd[$i + 1] == ',' || $cmd[$i + 1] == ')' || preg_match('#,$#', $cmd[$i]))
+	    {
+	      if ($j == 1 && test_type($cmd[$i]))
+		{
+		  $dbline .= $cmd[$i] . ',;';
+		}
+	      elseif (test_option($cmd[$i], 1))
+		$dbline .= $cmd[$i] . ';';
+	      $j = 0;
+	    }
 	  elseif (test_type($cmd[$i]))
 	    $dbline .= $cmd[$i] . ',';
 	  else
@@ -57,8 +99,11 @@ function	createTab($cmd, $file)
 	      echo "Invalid commande: '" . $cmd[$i] . "'\n";
 	      return ;
 	    }
+	  $j++;
 	}
     }
+  if (test_option(' ', 0))
+    return ;
   $dbline .= './database/table/' . $file_name . '#';
   if (isset($tbline))
     write_tb($file_name, $tbline);
