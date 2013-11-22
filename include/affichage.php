96708d382ed0e1bb1590bd462bb71c4047e5ae87
northvik <camillepire@northvik.fr>
2013-11-22 10:31:21 +0100

    aff

--- include/affichage.php
+++ include/affichage.php
@@ -5,7 +5,7 @@
 // Login   <carrie_v@etna-alternance.net>
 //
 // Started on  Mon Nov 18 10:31:24 2013 Valentin Carriere
-// Last update Fri Nov 22 10:22:11 2013 Valentin Carriere
+// Last update Fri Nov 22 10:30:03 2013 camille pire
 //
 
 function        aff_prompt()
@@ -18,7 +18,7 @@ function	aff_echo($str)
   global $option;
   if ($option != null)
     {
-      $fd = fopen($option, 'w+');
+      $fd = fopen($option, 'a+');
       fwrite($fd, $str);
       fclose($fd);
     }
